b18b73e78e68a75d9b783a3255d23ac28f79326c
Shawn Caeiro <shawncaeiro@gmail.com>
2016-01-21 23:41:05 -0600

    Comments Added

--- mHealthProject/HealthViewController.swift
+++ mHealthProject/HealthViewController.swift
@@ -13,30 +13,37 @@ import Parse
 
 class HealthViewController: UIViewController, UITextFieldDelegate {
 
+    // Initial step values
     var step_threshold = 7500
     var step_current = 0
     
+    // Classes for retrieve step sensor data
     let activityManager = CMMotionActivityManager()
     let pedometer = CMPedometer()
     
+    // Date Retrival
     let startOfToday = NSCalendar.currentCalendar().startOfDayForDate(NSDate())
     
+    // Label initializers
     @IBOutlet weak var thresholdLabel: UILabel!
     @IBOutlet weak var sayingLabel: UILabel!
-    
     @IBOutlet weak var stepCounter: UILabel!
-    @IBOutlet weak var manualStepEntry: UITextField!
     @IBOutlet weak var hwLabel: UILabel!
-    var stepVal: Int?
     
+    // Text Field Initializer
+    @IBOutlet weak var manualStepEntry: UITextField!
+    
+    // Run on App open
     override func viewDidLoad() {
         super.viewDidLoad()
         self.manualStepEntry.delegate = self;
         writeThreshold()
     }
     
+    // Run when Health View appears
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        // Begin retrieving sensor data
         getPedometerData()
     }
     
@@ -46,43 +53,54 @@ class HealthViewController: UIViewController, UITextFieldDelegate {
     }
     
     func getPedometerData() {
+        // Attempt to begin to receive continuous sensor data
+        // Data will be sent from sensors
+        // Every time data is sent, the dispatch_async portion of the code is run
+        
         pedometer.startPedometerUpdatesFromDate(startOfToday) { data, error in
             if let data = data {
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     self.stepCounter.font = self.stepCounter.font.fontWithSize(24)
-                    print("Steps Taken: \(data.numberOfSteps)")
+                    // Updates UI step count
                     self.updateStepCount(Int(data.numberOfSteps))
+                    // check is Hello or World should be shown
                     self.updateHello()
+                    // POST data to databse
                     self.postData(data)
                 })
             }
             else {
+                // If data is not found, display info on how to allow Step Tracking Access
                 self.stepCounter.font = self.stepCounter.font.fontWithSize(12)
                 self.stepCounter.text = "Please allow access to 'Motion & Fitness' for mHealthTracker in iPhone settings and then restart app. Otherwise, app is useless."
             }
         }
     }
     
-    func writeThreshold(){
-        thresholdLabel.text = "Goal: \(step_threshold) steps"
-    }
-    
-    func updateThreshold(){
-        let newThreshold = Int(manualStepEntry.text!)
+    func postData(stepData: CMPedometerData) {
+        // Function to POST data from sensor to database
         
-        if newThreshold != nil {
-            step_threshold = newThreshold!
-            writeThreshold()
+        // Serialize data into a PF object
+        let stepResults = PFObject(className:"healthTracker")
+        stepResults["startDate"] = stepData.startDate
+        stepResults["endDate"] = stepData.endDate
+        stepResults["steps"] = stepData.numberOfSteps
+        
+        // POST PF Object data to backend
+        stepResults.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                // The object has been saved.
+            } else {
+                // There was a problem, check error.description
+            }
         }
-        manualStepEntry.text = ""
-    }
-    
-    func updateStepCount(step: Int) {
-        step_current = step
-        stepCounter.text = "\(step) steps today!"
     }
     
     func updateHello(){
+        // Conditional logic for whether to display HELLO or WORLD
+        // Depending on the step count so far today
+        
         if step_current >= step_threshold{
             hwLabel.text = "WORLD"
             hwLabel.textColor = UIColor.greenColor()
@@ -95,31 +113,41 @@ class HealthViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func postData(stepData: CMPedometerData) {
-        let stepResults = PFObject(className:"healthTracker")
-        stepResults["startDate"] = stepData.startDate
-        stepResults["endDate"] = stepData.endDate
-        stepResults["steps"] = stepData.numberOfSteps
-        stepResults.saveInBackgroundWithBlock {
-            (success: Bool, error: NSError?) -> Void in
-            if (success) {
-                // The object has been saved.
-            } else {
-                // There was a problem, check error.description
-            }
-        }
+    func writeThreshold(){
+        // Function to display new Threshold to UI
+        
+        thresholdLabel.text = "Goal: \(step_threshold) steps"
     }
     
-
+    func updateThreshold(){
+        // Function process manual Threshold entry
+        
+        // Get input from Text Field
+        let newThreshold = Int(manualStepEntry.text!)
+        
+        // Check if the input was an Int.
+        // If if is not, newThreshold will be nil and this code won't run
+        if newThreshold != nil {
+            step_threshold = newThreshold!
+            writeThreshold()
+        }
+        manualStepEntry.text = ""
+    }
     
     func textFieldShouldReturn(manualStepEntry: UITextField) -> Bool {
+        // Function to update data once manual threshold is entered.
         self.view.endEditing(true)
         updateThreshold()
         updateHello()
         return false
     }
     
-
+    func updateStepCount(step: Int) {
+        // Function to display new step count to UI
+        
+        step_current = step
+        stepCounter.text = "\(step) steps today!"
+    }
     
     /*
     // MARK: - Navigation
